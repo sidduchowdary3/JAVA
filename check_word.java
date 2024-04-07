f71811579088096478b8275c8945edf6f10f7216
sidduchowdary3 <164843613+sidduchowdary3@users.noreply.github.com>
2024-04-07 12:14:35 +0530

    Create check_word.java

---
+++ check_word.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Main 
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        String a=s.next();
+        System.out.println(a.contains("orange"));
+    }
+}
