3da944dc2461c85eccf87afa39aebc112a4b51b6
sidduchowdary3 <164843613+sidduchowdary3@users.noreply.github.com>
2024-04-07 12:04:17 +0530

    Create printatring.java

---
+++ printatring.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String args[]) {
+        Scanner s = new Scanner(System.in);
+        String sa = s.next();
+        System.out.println(sa);
+    }
+}
